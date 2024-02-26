8a94b689c44099f776fb40ad8a0e29fae5c1da5e
Abraham Gutierrez <labraham1986@gmail.com>
2024-02-26 13:06:05 -0500

    add not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', component:HomeComponent, title:'FakeStore Home Page' },
+  { path: 'home', component:HomeComponent,title:'FakeStore Home Page' },
   { path: 'product/:id', component:DetailProductComponent, title: 'Detail Product' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent},
 ];
